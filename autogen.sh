cfc8553c51bac2797ad704e1d45ec60a5ceb4a0c
Jody Goldberg <jody@src.gnome.org>
2005-02-28 03:21:27 +0000

    Some initial cheesy copies to move closer to compiling

---
+++ autogen.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Run this to generate all the initial makefiles, etc.
+
+srcdir=`dirname $0`
+test -z "$srcdir" && srcdir=.
+
+PKG_NAME="goffice"
+REQUIRED_LIBTOOL_VERSION=1.4.3
+
+(test -f $srcdir/configure.in \
+  && test -f $srcdir/goffice.h) || {
+    echo -n "**Error**: Directory "\`$srcdir\'" does not look like the"
+    echo " top-level goffice directory"
+    exit 1
+}
+
+ifs_save="$IFS"; IFS=":"
+for dir in $PATH ; do
+  test -z "$dir" && dir=.
+  if test -f $dir/gnome-autogen.sh ; then
+    gnome_autogen="$dir/gnome-autogen.sh"
+    gnome_datadir=`echo $dir | sed -e 's,/bin$,/share,'`
+    break
+  fi
+done
+IFS="$ifs_save"
+
+if test -z "$gnome_autogen" ; then
+  echo "You need to install the gnome-common module and make"
+  echo "sure the gnome-autogen.sh script is in your \$PATH."
+  exit 1
+fi
+
+REQUIRED_AUTOMAKE_VERSION=1.7.2 GNOME_DATADIR="$gnome_datadir" USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . $gnome_autogen
