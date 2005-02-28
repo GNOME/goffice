46ee4ea00c4dc58867bbcc42574c1c75e21aae2a
Christopher James Lahey <clahey@clahey.net>
2005-02-28 05:39:03 +0000

    Changed gnumeric to goffice here.
    2005-02-28  Christopher James Lahey  <clahey@clahey.net>
    
    	* configure.in, goffice-plugins.mk: Changed gnumeric to goffice
    	here.

--- goffice-plugins.mk
+++ goffice-plugins.mk
@@ -4,6 +4,6 @@ INCLUDES = \
     -I$(top_builddir)/src				\
     -I$(top_srcdir)/src/cut-n-paste-code		\
     -I$(top_srcdir)/src/cut-n-paste-code/foocanvas	\
-    $(GNUMERIC_CFLAGS)
+    $(GOFFICE_CFLAGS)
 
-GOFFICE_PLUGIN_FLAGS = $(GNUMERIC_PLUGIN_LDFLAGS)
+GOFFICE_PLUGIN_FLAGS = $(GOFFICE_PLUGIN_LDFLAGS)
