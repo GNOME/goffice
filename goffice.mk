7c7408b0de327f706eb274dbb826b5c634b20737
Stepan Kasal <skasal@src.gnome.org>
2005-04-01 12:04:23 +0000

    autoconfigury changes

--- goffice.mk
+++ goffice.mk
@@ -1,6 +1,7 @@
-INCLUDES = -I$(top_builddir)			\
-	   -I$(top_srcdir)			\
-	    $(GOFFICE_CFLAGS)
+AM_CPPFLAGS = \
+	-I$(top_builddir)		\
+	-I$(top_srcdir)			\
+	$(GOFFICE_CFLAGS)
 
 GOFFICE_PLUGIN_FLAGS = $(GOFFICE_PLUGIN_LDFLAGS)
 goffice_include_dir = $(includedir)/libgoffice-1/goffice
