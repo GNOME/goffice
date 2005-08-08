b91f01012b2e3cd5f6241e369b64a8a3619e9d03
J.H.M. Dassen (Ray) <jdassen@src.gnome.org>
2005-08-08 08:57:07 +0000

    Updated FSF's address

--- goffice/ms-compat/go-ms-parser.c
+++ goffice/ms-compat/go-ms-parser.c
@@ -16,10 +16,10 @@
  * Library General Public License for more details.
  *
  * You should have received a copy of the GNU Library General Public
- * License along with this file; if not, write to
- * the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
- **/
+ * License along with this file; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301
+ * USA.
+ */
 
 #include <goffice/goffice-config.h>
 #include <goffice/ms-compat/go-ms-parser.h>
