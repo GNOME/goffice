f9284faef971d447c72b626fac053d6c41bcfdf8
Stepan Kasal <skasal@src.gnome.org>
2005-12-16 19:21:15 +0000

    Add explantions about pkg-config versioning.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,6 @@ PKG_NAME="goffice"
 
 REQUIRED_AUTOMAKE_VERSION=1.7.2
 REQUIRED_LIBTOOL_VERSION=1.4.3
-#REQUIRED_PKG_CONFIG_VERSION=0.18.0
 
 # We need intltool >= 0.27.2 to extract the UTF-8 chars from source code:
 REQUIRED_INTLTOOL_VERSION=0.27.2
@@ -13,6 +12,20 @@ REQUIRED_INTLTOOL_VERSION=0.27.2
 # We require Automake 1.7.2, which requires Autoconf 2.54.
 # (It needs _AC_AM_CONFIG_HEADER_HOOK, for example.)
 REQUIRED_AUTOCONF_VERSION=2.54
+
+# This enforces the version for the machine where the tarball is built
+# from CVS.  This requirement might be stronger than the one in configure.in.
+#
+# An example: we might require pkg-config binary >= 0.18, because it
+# implements Libs.private.  But we might require macro PKG_PROG_PKG_CONFIG
+# from pkg-config >= 0.29 because this macro is able to detect old versions
+# of pkg-config conrrectly and reports a sane error message.
+#
+# In other words, the distribution tarball can be created only with 0.29 or
+# later, but it can be then compiled on any host with pkg-config >= 0.18.
+#
+# We would then use REQUIRED_PKG_CONFIG_VERSION=0.29 here and
+# PKG_PROG_PKG_CONFIG([0.18]) in configure.in.
 
 USE_GNOME2_MACROS=1
 USE_COMMON_DOC_BUILD=yes
