ac718ded7f47563254d6034d421a926f441ea7fe
Morten Welinder <terra@gnome.org>
2007-04-02 14:59:11 +0000

    Undo last change.
    2007-04-02  Morten Welinder  <terra@gnome.org>
    
    	* goffice/utils/regutf8.c (go_regcomp): Undo last change.
    
    
    svn path=/trunk/; revision=1718

--- goffice/utils/regutf8.c
+++ goffice/utils/regutf8.c
@@ -65,7 +65,6 @@ int
 go_regcomp (GORegexp *gor, const char *pat , int cflags)
 {
   const char *errorptr;
-  char *norm_pat;
   int errorofs, errorcode;
   pcre *r;
   int coptions =
@@ -74,11 +73,9 @@ go_regcomp (GORegexp *gor, const char *pat , int cflags)
     ((cflags & REG_ICASE) ? PCRE_CASELESS : 0) |
     ((cflags & REG_NEWLINE) ? PCRE_MULTILINE : 0);
 
-  norm_pat = g_utf8_normalize (pat, -1, G_NORMALIZE_DEFAULT);
-  gor->ppcre = r = pcre_compile2 (norm_pat, coptions,
+  gor->ppcre = r = pcre_compile2 (pat, coptions,
 				  &errorcode, &errorptr, &errorofs,
 				  NULL);
-  g_free (norm_pat);
 
   if (r == NULL) {
     switch (errorcode) {
