b501f769debab1fffd87eae33039f47aea63a3fb
Jean Bréfort <jbrefort@src.gnome.org>
2008-04-05 17:17:27 +0000

    relicensed to gpl-v2+
    svn path=/trunk/; revision=2051

--- goffice/math/go-cspline.c
+++ goffice/math/go-cspline.c
@@ -5,11 +5,12 @@
  * The code in this file has been adapted from public domain code by 
  * Sylvain BARTHELEMY (http://www.sylbarth.com/interp.php).
  *
- * Copyright (C) 2007 Jean Brefort (jean.brefort@normalesup.org)
+ * Copyright (C) 2007-2008 Jean Brefort (jean.brefort@normalesup.org)
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of version 2 of the GNU General Public
- * License as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
--- goffice/math/go-cspline.h
+++ goffice/math/go-cspline.h
@@ -2,11 +2,12 @@
 /*
  * go-cspline.h
  *
- * Copyright (C) 2007 Jean Brefort (jean.brefort@normalesup.org)
+ * Copyright (C) 2007-2008 Jean Brefort (jean.brefort@normalesup.org)
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of version 2 of the GNU General Public
- * License as published by the Free Software Foundation.
+ * This program is free software; you can redistribute it and/or 
+ * modify it under the terms of the GNU General Public License as 
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
