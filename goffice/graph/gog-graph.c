c19f93360025f4f907b30ad6b10306ce1750ecb6
Jean Brefort <jean.brefort@normalesup.org>
2008-11-10 14:10:35 +0000

    fixed typo.
    2008-11-10  Jean Brefort  <jean.brefort@normalesup.org>
    
    	* goffice/graph/gog-graph.c: (gog_graph_set_property): fixed typo.
    
    
    svn path=/trunk/; revision=2270

--- goffice/graph/gog-graph.c
+++ goffice/graph/gog-graph.c
@@ -92,7 +92,7 @@ gog_graph_set_property (GObject *obj, guint param_id,
 		GObject *object = g_value_get_object (value);
 		if ((GODoc *) object == graph->doc)
 			break;
-		if (IS_GO_DOC (obj)) {
+		if (IS_GO_DOC (object)) {
 			graph->doc = (GODoc *) object;
 			gog_object_document_changed (GOG_OBJECT (graph), graph->doc);
 		}
