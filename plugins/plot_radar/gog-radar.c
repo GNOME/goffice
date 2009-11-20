640c38228b1f85d86dae2ab99b36330488fc668e
Jean Brefort <jean.brefort@normalesup.org>
2009-11-20 20:47:44 +0100

    Fixed a ui file name.

--- plugins/plot_radar/gog-radar.c
+++ plugins/plot_radar/gog-radar.c
@@ -488,7 +488,7 @@ gog_color_polar_plot_populate_editor (GogObject *obj,
 	char *path;
 
 	dir = go_plugin_get_dir_name (go_plugins_get_plugin_by_id ("GOffice_plot_radar"));
-	path = g_build_filename (dir, "gog-color-polar-prefs.xml", NULL);
+	path = g_build_filename (dir, "gog-color-polar-prefs.ui", NULL);
 	gui = go_gtk_builder_new (path, GETTEXT_PACKAGE, cc);
 	g_free (path);
 
