0cb2eaf35f44955aa7f25a6b8f2142c6a0686c63
Jean Brefort <jean.brefort@normalesup.org>
2009-12-09 20:58:58 +0100

    more leaks fixed.

--- goffice/graph/gog-grid-line.c
+++ goffice/graph/gog-grid-line.c
@@ -267,22 +267,28 @@ gog_grid_line_xyz_render (GogGridLine *grid_line, GogView *view,
 	if (axis_type == GOG_AXIS_X) {
 		axes  = gog_chart_get_axes (chart, GOG_AXIS_Y);
 		axis1 = GOG_AXIS (axes->data);
+		g_slist_free (axes);
 		axes  = gog_chart_get_axes (chart, GOG_AXIS_Z);
 		axis2 = GOG_AXIS (axes->data);
+		g_slist_free (axes);
 		c_map = gog_chart_map_3d_new (chart, plot_area,
 			axis, axis1, axis2);
 	} else if (axis_type == GOG_AXIS_Y) {
 		axes  = gog_chart_get_axes (chart, GOG_AXIS_Z);
 		axis1 = GOG_AXIS (axes->data);
+		g_slist_free (axes);
 		axes  = gog_chart_get_axes (chart, GOG_AXIS_X);
 		axis2 = GOG_AXIS (axes->data);
+		g_slist_free (axes);
 		c_map = gog_chart_map_3d_new (chart, plot_area,
 			axis2, axis, axis1);
 	} else {
 		axes  = gog_chart_get_axes (chart, GOG_AXIS_X);
 		axis1 = GOG_AXIS (axes->data);
+		g_slist_free (axes);
 		axes  = gog_chart_get_axes (chart, GOG_AXIS_Y);
 		axis2 = GOG_AXIS (axes->data);
+		g_slist_free (axes);
 		c_map = gog_chart_map_3d_new (chart, plot_area,
 			axis1, axis2, axis);
 	}
