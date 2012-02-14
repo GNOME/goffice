57d7e1dfef846f3e645466e2521568eaea680ea0
Morten Welinder <terra@gnome.org>
2012-02-14 14:32:51 -0500

    Compilation: create a list of sources.
    This is in preparation for introspection work.

--- goffice.mk
+++ goffice.mk
@@ -9,3 +9,17 @@ GOFFICE_PLUGIN_FLAGS = $(GOFFICE_PLUGIN_LDFLAGS)
 goffice_include_dir = $(includedir)/libgoffice-@GOFFICE_API_VER@/goffice
 
 include $(top_srcdir)/goffice-win32.mk
+
+all-local: list_of_sources
+
+list_of_sources:
+	rm -f $@.tmp
+	touch $@.tmp
+	for i in $(SOURCES) $(HEADERS); do \
+		echo $$i | grep '\.[ch]$$' >> $@.tmp; \
+	done
+	test -z "${SUBDIRS}" || for s in ${SUBDIRS}; do \
+		test -e $$s/list_of_sources && \
+		perl -pe "s{^}{$$s/}" $$s/list_of_sources >> $@.tmp; \
+	done
+	mv $@.tmp $@
