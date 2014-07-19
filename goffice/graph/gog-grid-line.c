1a45c2b6ffb5f25a4f3159a2123bb541f4b01688
Jean Brefort <jean.brefort@normalesup.org>
2014-07-19 18:51:20 +0200

    Fix grid line theme support. [#733403]

--- goffice/graph/gog-grid-line.c
+++ goffice/graph/gog-grid-line.c
@@ -57,7 +57,7 @@ gog_grid_line_init_style (GogStyledObject *gso, GOStyle *style)
 {
 	style->interesting_fields = GO_STYLE_LINE | GO_STYLE_FILL;
 	gog_theme_fillin_style (gog_object_get_theme (GOG_OBJECT (gso)),
-		style, GOG_OBJECT (gso), 0, FALSE);
+		style, GOG_OBJECT (gso), 0, GO_STYLE_LINE | GO_STYLE_FILL);
 }
 
 static void
