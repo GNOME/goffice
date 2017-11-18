275ad7c73737dd2194e800cc80f9c91fda29e4ea
Anders Jonsson <anders.jonsson@norsjovallen.se>
2017-11-18 13:57:55 +0100

    Replace old FSF addresses with link
    https://bugzilla.gnome.org/show_bug.cgi?id=790534

--- plugins/lasem/component.c
+++ plugins/lasem/component.c
@@ -15,9 +15,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
- * USA
+ * along with this program; if not, see <https://www.gnu.org/licenses/>.
  */
 
 #include <goffice/goffice-config.h>
@@ -44,9 +42,7 @@
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place, Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Public License along with this library; if not, see <https://www.gnu.org/licenses/>.
  *
  * Author:
  * 	Emmanuel Pacaud <emmanuel@gnome.org>
